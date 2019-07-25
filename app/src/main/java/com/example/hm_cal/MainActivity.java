a673a577479eb426671636defb494d7527905b4f
jhhuang7 <juhua.huang@uqconnect.edu.au>
2019-07-25 19:21:33 +1000

    I've done goofed

--- app/src/main/java/com/example/hm_cal/MainActivity.java
+++ app/src/main/java/com/example/hm_cal/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity {
 
     // The equation being built by pressing keys
@@ -95,49 +97,49 @@ public class MainActivity extends AppCompatActivity {
     public void btnPress(View view){
         switch (view.getId()) {
             case R.id.btn_0:
-                equation.append("0");
+                equation.append("0 ");
                 break;
             case R.id.btn_1:
-                equation.append("1");
+                equation.append("1 ");
                 break;
             case R.id.btn_2:
-                equation.append("2");
+                equation.append("2 ");
                 break;
             case R.id.btn_3:
-                equation.append("3");
+                equation.append("3 ");
                 break;
             case R.id.btn_4:
-                equation.append("4");
+                equation.append("4 ");
                 break;
             case R.id.btn_5:
-                equation.append("5");
+                equation.append("5 ");
                 break;
             case R.id.btn_6:
-                equation.append("6");
+                equation.append("6 ");
                 break;
             case R.id.btn_7:
-                equation.append("7");
+                equation.append("7 ");
                 break;
             case R.id.btn_8:
-                equation.append("8");
+                equation.append("8 ");
                 break;
             case R.id.btn_9:
-                equation.append("9");
+                equation.append("9 ");
                 break;
             case R.id.btn_decimal:
-                equation.append(".");
+                equation.append(". ");
                 break;
             case R.id.btn_plus:
-                equation.append("+");
+                equation.append("+ ");
                 break;
             case R.id.btn_subtract:
-                equation.append("-");
+                equation.append("- ");
                 break;
             case R.id.btn_multiply:
-                equation.append("×");
+                equation.append("× ");
                 break;
             case R.id.btn_divide:
-                equation.append("÷");
+                equation.append("÷ ");
                 break;
         }
 
@@ -161,6 +163,114 @@ public class MainActivity extends AppCompatActivity {
 
     public void calculate(View view) {
         TextView answer = (TextView) findViewById(R.id.textView);
-        answer.setText("Yeah but nah!");
+        System.out.println(equation.toString());
+        answer.setText("I've done goofed");
+
+
+    }
+
+    // Shit happens
+    public static int evaluate(String expression)
+    {
+        char[] tokens = expression.toCharArray();
+
+        // Stack for numbers: 'values'
+        Stack<Integer> values = new Stack<Integer>();
+
+        // Stack for Operators: 'ops'
+        Stack<Character> ops = new Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++)
+        {
+            // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+                continue;
+
+            // Current token is a number, push it to stack for numbers
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                StringBuffer sbuf = new StringBuffer();
+                // There may be more than one digits in number
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.toString()));
+            }
+
+            // Current token is an opening brace, push it to 'ops'
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+
+                // Closing brace encountered, solve entire brace
+            else if (tokens[i] == ')')
+            {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+
+            // Current token is an operator.
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '×' || tokens[i] == '÷')
+            {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+        // Top of 'values' contains result, return it
+        return values.pop();
+    }
+
+    // Returns true if 'op2' has higher or same precedence as 'op1',
+    // otherwise returns false.
+    public static boolean hasPrecedence(char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '×' || op1 == '÷') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    // A utility method to apply an operator 'op' on operands 'a'
+    // and 'b'. Return the result.
+    public static int applyOp(char op, int b, int a)
+    {
+        switch (op)
+        {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '×':
+                return a * b;
+            case '÷':
+                if (b == 0)
+                    throw new
+                            UnsupportedOperationException("Cannot divide by zero");
+                return a / b;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evaluate("10 + 2 × 6"));
+        System.out.println(evaluate("100 × 2 + 12"));
+        System.out.println(evaluate("100 × ( 2 + 12 )"));
+        System.out.println(evaluate("100 × ( 2 + 12 ) ÷ 14"));
     }
 }
+
+
